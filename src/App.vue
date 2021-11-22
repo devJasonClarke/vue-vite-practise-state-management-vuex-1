1081ab9c25d6f978d03dc7b65d0f055475d9296b
devJasonClarke <jettison200@gmail.com>
2021-11-22 16:37:39 -0500

    ⚡ Got Vuex up and running ⚡

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <div class="todos-container">
+      <p>Todos From Vuex:</p>
+      <ul>
+        <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["todos"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todos-container {
+  margin-top: 30px;
+}
+</style>
